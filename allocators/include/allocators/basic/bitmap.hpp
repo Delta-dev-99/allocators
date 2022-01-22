99a2755a17307c6b58a7269c0d830230389ffef2
deltadev99 <delta.deltadev.127@gmail.com>
2022-01-22 05:54:42 -0500

    Separated data structure of bitmap allocator

--- allocators/include/allocators/basic/bitmap.hpp
+++ allocators/include/allocators/basic/bitmap.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <allocators/basic/allocator.hpp>
+#include <allocators/structures/bitmap.hpp>
+
 #include <limits>
 #include <bit>
 
@@ -9,29 +11,19 @@
 namespace dd99::memory::block_allocator
 {
     // Allocates fixed-size blocks
+    // Uses some of the memory to keep a bitmap
     template <std::size_t Block_Size, class Bitmap_Element_T = std::uint8_t>
     class Bitmap : public Allocator
     {
-        using BMP_t = Bitmap_Element_T;
+        using BMP = dd99::memory::structure::Bitmap<Bitmap_Element_T>;
 
     public:
         Bitmap(const memory::Block &memory)
             : m_memory(memory)
+            , m_block_count(block_count(memory.size))
+            , m_bitmap(m_block_count, memory.base)
+            , m_blocks_base(reinterpret_cast<void *>(reinterpret_cast<std::uintptr_t>(m_memory.get_end()) - m_block_count * Block_Size))
         {
-            // worst case block count: bitmap is full unused memory is just below 1 block and 1 new bitmap element
-            const auto worst_case_unused = Block_Size + sizeof(BMP_t) - 1;
-            m_block_count = (n_bits * (m_memory.size - worst_case_unused)) / (n_bits * Block_Size + 1);
-            // if bitmap not full, there is a last, partially unused, element that division does not account for.
-            m_bitmap_size = m_block_count / n_bits + (bitmap_full() ? 0 : 1);
-
-            // if not worst case, we can use one more block
-            const auto unused = m_memory.size - m_bitmap_size * sizeof(BMP_t) - m_block_count * Block_Size;
-            if ((unused >= Block_Size) && !bitmap_full())
-                ++m_block_count;
-
-            // set the base address of the blocks
-            m_blocks = reinterpret_cast<void *>(reinterpret_cast<std::uintptr_t>(m_memory.get_end()) - m_block_count * Block_Size);
-
             // mark all blocks as free
             deallocate_all();
         }
@@ -42,37 +34,10 @@ namespace dd99::memory::block_allocator
             // larger allocations not supported
             if (requested_size > Block_Size)
                 return {};
-            
-            // group bitmap elements (should be of some padding-free unsigned integer type) in blocks for faster iteration
-            using Block_t = std::uint_fast32_t;
-            const auto n_blocks = m_bitmap_size * sizeof(BMP_t) / sizeof(Block_t);
-            auto bmp = get_bitmap_ptr();
-            auto block_ptr = reinterpret_cast<Block_t *>(bmp);
 
-            std::size_t block_index = 0;
-            for (; block_index < n_blocks; ++block_index)
-            {
-                if (block_ptr[block_index] != Block_t(-1))
-                    break;
-            }
-
-            auto index = block_index * sizeof(Block_t) / sizeof(BMP_t);
-            for (; index < m_bitmap_size; ++index)
-            {
-                if (bmp[index] == BMP_t(-1))
-                    continue;
-
-                for (int bit = 0; bit < n_bits; ++bit)
-                {
-                    const auto mask = BMP_t(1) << bit;
-
-                    if ((bmp[index] & mask) == 0)
-                    {
-                        bmp[index] |= mask;
-                        return get_memory_block(index * n_bits + bit);
-                    }
-                }
-            }
+            const auto free_index = m_bitmap.set_first_unset();
+            if (free_index != decltype(free_index)(-1))
+                return get_memory_block(free_index);
 
             // no free block found
             return {};
@@ -85,29 +50,12 @@ namespace dd99::memory::block_allocator
             
             // mark block as free
             auto index = get_index(memory);
-            get_bitmap_ptr()[index / n_bits] &= ~(BMP_t(1) << (index % n_bits));
+            m_bitmap.clear(index);
         }
 
         void deallocate_all()
         {
-            // reset the bitmap state
-            // all blocks set to free
-            auto bmp = get_bitmap_ptr();
-            for (std::size_t index = 0; index < m_bitmap_size; index++)
-            {
-                bmp[index] = 0;
-            }
-
-            // bits not mapped to blocks
-            // set as used
-            if (!bitmap_full())
-            {
-                // number of bits on the last bitmap element that are mapped to memory
-                const auto n_last_bits = m_block_count % n_bits;
-                const auto n_unused_bits = n_bits - n_last_bits;
-
-                bmp[m_bitmap_size - 1] |= ((BMP_t(1) << n_unused_bits) - 1) << n_last_bits;
-            }
+            m_bitmap.reset();
         }
 
         bool owns(const memory::Block &memory) const
@@ -120,38 +68,42 @@ namespace dd99::memory::block_allocator
             return m_memory.contains(memory);
         }
 
+    public:
+        constexpr static auto block_count(std::size_t memory_size)
+        {
+            // worst case block count: bitmap is full unused memory is just below 1 block and 1 new bitmap element
+            const auto worst_case_unused = Block_Size + BMP::Block_Size - 1;
+            auto block_count = (BMP::Block_Bits * (memory_size - worst_case_unused)) / (BMP::Block_Bits * Block_Size + 1);
+            const auto unused = memory_size - BMP::size(block_count) * BMP::Block_Size - block_count * Block_Size;
+            
+            if (unused >= Block_Size && !BMP::fully_mapped(block_count))
+                ++block_count;
+            
+            return block_count;
+        }
+
     private:
         memory::Block m_memory;
         std::size_t m_block_count;
-        std::size_t m_bitmap_size; // number of elements in bitmap
-        void *m_blocks;
-        static constexpr auto n_bits = std::numeric_limits<BMP_t>::digits;
-        
-        // get ptr to start of bitmap.
-        // this function allows to change the bitmap position without affecting logic
-        BMP_t *get_bitmap_ptr()
-        {
-            // bitmap located at the base of the memory
-            return  reinterpret_cast<BMP_t *>(m_memory.base);
-        }
+        // std::size_t m_bitmap_size; // number of elements in bitmap
+        void *m_blocks_base;
+        // static constexpr auto n_bits = std::numeric_limits<BMP_t>::digits;
 
-        // whether all bitmap bits are mapped to memory blocks
-        bool bitmap_full() const
-        {
-            return m_block_count % n_bits == 0;
-        }
+        BMP m_bitmap;
+
+
 
         // traduce index to memory block
         Block get_memory_block(std::size_t index)
         {
-            const auto ptr = reinterpret_cast<void *>(reinterpret_cast<std::uintptr_t>(m_blocks) + Block_Size * index);
+            const auto ptr = reinterpret_cast<void *>(reinterpret_cast<std::uintptr_t>(m_blocks_base) + Block_Size * index);
             return {.base = ptr, .size = Block_Size};
         }
 
         // traduce memory block to bitmap index
         std::size_t get_index(const memory::Block& memory)
         {
-            return (reinterpret_cast<std::uintptr_t>(memory.base) - reinterpret_cast<std::uintptr_t>(m_blocks)) / Block_Size;
+            return (reinterpret_cast<std::uintptr_t>(memory.base) - reinterpret_cast<std::uintptr_t>(m_blocks_base)) / Block_Size;
         }
     };
 }
---
+++ allocators/include/allocators/structures/bitmap.hpp
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <cstdint>
+#include <limits>
+
+namespace dd99::memory::structure
+{
+    template <class Block_T = std::uint8_t>
+    class Bitmap
+    {
+    public:
+        constexpr static auto Block_Bits = std::numeric_limits<Block_T>::digits;
+        constexpr static auto Block_Size = sizeof(Block_T);
+
+    public: // statics
+        // All blocks are fully used?
+        constexpr static bool fully_mapped(std::size_t bit_count)
+        {
+            return bit_count % Block_Bits == 0;
+        }
+
+        // The number of Bitmap Blocks used
+        // valid for bit_count > 0
+        constexpr static std::size_t size(std::size_t bit_count)
+        {
+            return  (bit_count - 1) / Block_Bits + 1;
+        }
+        
+    public:
+        // Create the bitmap in-place
+        Bitmap(std::size_t bit_count, void *base)
+            : m_base(reinterpret_cast<Block_T *>(base))
+            , m_bit_size(bit_count)
+        {
+            m_size = Bitmap::size(bit_count);
+        }
+
+    public:
+        bool fully_mapped() const
+        {
+            return fully_mapped(m_bit_size);
+        }
+
+        std::size_t size() const
+        {
+            return m_size;
+        }
+
+        std::size_t bit_size() const
+        {
+            return m_bit_size;
+        }
+
+
+
+        void set(std::size_t bit_index)
+        {
+            m_base[bit_index / Block_Bits] |= (Block_T(1) << (bit_index % Block_Bits));
+        }
+
+        void clear(std::size_t bit_index)
+        {
+            m_base[bit_index / Block_Bits] &= ~(Block_T(1) << (bit_index % Block_Bits));
+        }
+
+        // returns the index of the set bit, or -1 if not found
+        std::size_t set_first_unset()
+        {
+            // optimization using larger type for iteration
+            using Fast_Block = std::uint_fast32_t;
+            const auto n_fast_blocks = bit_size() / std::numeric_limits<Fast_Block>::digits;
+            /* volatile */ auto fast_block_ptr = reinterpret_cast<Fast_Block *>(m_base);
+            const auto fast_block_ptr_end = reinterpret_cast<Fast_Block *>(m_base) + n_fast_blocks;
+            while (fast_block_ptr < fast_block_ptr_end)
+            {
+                if (*fast_block_ptr != Fast_Block(-1))
+                    break;
+
+                ++fast_block_ptr;
+            }
+
+            auto block_ptr = reinterpret_cast<Block_T *>(fast_block_ptr);
+            const auto block_ptr_end = m_base + size();
+            while (block_ptr < block_ptr_end)
+            {
+                if (*block_ptr != Block_T(-1))
+                {
+                    for (int bit = 0; bit < Block_Bits; ++bit)
+                    {
+                        const auto mask = Block_T(1) << bit;
+
+                        if ((*block_ptr & mask) == 0)
+                        {
+                            *block_ptr |= mask;
+                            return (block_ptr - m_base) * Block_Bits + bit;
+                        }
+                    }
+                }
+
+                ++block_ptr;
+            }
+
+            return -1;
+        }
+
+        // clear all mapped bits, set all unmapped bits
+        void reset()
+        {
+            // optimization using larger type for iteration
+            using Fast_Block = std::uint_fast32_t;
+            const auto n_fast_blocks = bit_size() / std::numeric_limits<Fast_Block>::digits;
+            /* volatile */ auto fast_block_ptr = reinterpret_cast<Fast_Block *>(m_base);
+            const auto fast_block_ptr_end = reinterpret_cast<Fast_Block *>(m_base) + n_fast_blocks;
+            while (fast_block_ptr < fast_block_ptr_end)
+            {
+                *fast_block_ptr++ = 0;
+            }
+
+            // clear the rest of blocks
+            /* volatile */ auto block_ptr = reinterpret_cast<Block_T *>(fast_block_ptr);
+            const auto block_ptr_end = m_base + size();
+            while (block_ptr < block_ptr_end)
+            {
+                *block_ptr++ = 0;
+            }
+
+            // mark unmapped bits as used
+            // last block only
+            if (!fully_mapped())
+            {
+                const auto n_used_bits = bit_size() % Block_Bits;
+                const auto n_unused_bits = Block_Bits - n_used_bits;
+
+                m_base[size() - 1] |= ((Block_T(1) << n_unused_bits) - 1) << n_used_bits;
+            }
+        }
+
+        // get the value of a specific bit
+        bool operator[](std::size_t index) const
+        {
+            return m_base[index / Block_Bits] & (Block_T(1) << index % Block_Bits);
+        }
+
+    private:
+        Block_T *m_base;
+        std::size_t m_bit_size;
+        std::size_t m_size;
+    };
+}
