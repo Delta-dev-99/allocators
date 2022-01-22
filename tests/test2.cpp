6f41acb00699eccc86f12adbd17e44d3cb7a2141
deltadev99 <delta.deltadev.127@gmail.com>
2022-01-22 05:56:31 -0500

    Added some tests

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -10,6 +10,10 @@
 #include <allocators/basic/bitmap.hpp>
 #include <allocators/basic/buddy.hpp>
 
+#include <iostream>
+
+
+
 void do_something_with_allocator(dd99::memory::block_allocator::Allocator& alloc)
 {
     auto x1 = alloc.allocate(10);
@@ -72,10 +76,50 @@ void test1()
 void test2()
 {
     namespace allocs = dd99::memory::block_allocator;
+
+    dd99::memory::Self_Contained_Block<1024> my_memory;
+
+    allocs::Bitmap<256> bmp_alloc(my_memory);
+
+    auto x1 = bmp_alloc.allocate(100);
+    auto x2 = bmp_alloc.allocate(200);
+    auto x3 = bmp_alloc.allocate(300);
+    auto x4 = bmp_alloc.allocate(100);
+    auto x5 = bmp_alloc.allocate(100);
+
+    bmp_alloc.deallocate(x2);
+
+    auto x6 = bmp_alloc.allocate(10);
+    bmp_alloc.deallocate_all();
+}
+
+void test3()
+{
+    namespace allocs = dd99::memory::block_allocator;
+
+    dd99::memory::Self_Contained_Block<1024> my_memory;
+
+    allocs::Buddy<16, 3> buddy_alloc(my_memory);
+
+    // constexpr auto x = allocs::Buddy<16, 11>::block_count(1024 << 4);
+    // auto y = allocs::Buddy<16, 11>::block_count(100 * 1024);
+
+    using Buddy_t = allocs::Buddy<16, 2>;
+    
+    constexpr std::size_t mem_size = 100;
+    constexpr auto n = Buddy_t::block_count(mem_size);
+    constexpr auto bits = Buddy_t::bitmap_bits(n);
+    constexpr auto bmp = Buddy_t::BMP::size(n) * Buddy_t::BMP::Block_Size;
+    constexpr auto unused = mem_size - bmp - n * Buddy_t::Block_Size;
+    constexpr auto ratio = Buddy_t::ratio(n);
+
+    for (int i = 0; i < 100; i++)
+        std::cout << i << "\t" << Buddy_t::ratio(i) << "\n";
 }
 
 int main()
 {
     // test1();
-    test2();   
+    // test2();   
+    test3();
 }
