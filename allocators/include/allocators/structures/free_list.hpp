5bed3e6d6aeebb54a5cdd6387340efbfe5432624
deltadev99 <delta.deltadev.127@gmail.com>
2022-02-11 02:41:08 -0500

    Added doubly linked freelist

--- allocators/include/allocators/structures/free_list.hpp
+++ allocators/include/allocators/structures/free_list.hpp
@@ -75,4 +75,89 @@ namespace dd99::memory::structure
 
     };
 
+
+
+    // doubly linked list of free memory blocks that stores
+    // nodes on empty blocks
+    class Freelist_Double_Link
+    {
+    protected:
+        // Header for free blocks. The nodes of the freelist
+        struct Free_Block_Header { Free_Block_Header *next = nullptr; Free_Block_Header *prev = nullptr; };
+
+    private:
+        std::size_t m_block_size;
+        Free_Block_Header *first = nullptr;
+
+    public:
+        Freelist_Double_Link(std::size_t block_size)
+            : m_block_size(block_size)
+        {
+            if (sizeof(Free_Block_Header) > block_size)
+                throw std::length_error{"Freelist_Double_Link: block_size (" + std::to_string(block_size) + ") is too short. Min: " + std::to_string(sizeof(Free_Block_Header))};
+        }
+
+        Freelist_Double_Link(const Freelist_Double_Link &) = delete;
+        Freelist_Double_Link(Freelist_Double_Link &&other) = default;
+
+        ~Freelist_Double_Link()
+        {
+            // call destructor on created nodes
+            clear();
+        }
+
+
+    public:
+        [[nodiscard]]
+        memory::Block pop()
+        {
+            if (!first) return {};
+            
+            auto block_ptr = first;
+            first = first->next;
+            first->prev = nullptr;
+            block_ptr->~Free_Block_Header();
+            return {.base = block_ptr, .size = m_block_size};
+        }
+
+        void push(const memory::Block& block)
+        {
+            auto new_header = new (block.base) Free_Block_Header{.next = first};
+            first->prev = new_header;
+            first = new_header;
+        }
+
+        void remove(const memory::Block & block)
+        {
+            // NOTE: Assumed block is in the freelist
+            // UB otherwise.
+
+            auto header = reinterpret_cast<Free_Block_Header *>(block.base);
+            header->prev->next = header->next;
+            header->~Free_Block_Header();
+        }
+
+        void clear()
+        {
+            // NOTE: If the destruction of the block header is trivial,
+            // this function reduces to setting `first` to `nullptr`
+            // NOTE: I know destruction IS trivial, but compiler should know too.
+
+            auto current = first;
+            first = nullptr;
+            while (current)
+            {
+                const auto tmp = current->next;
+                current->~Free_Block_Header();
+                current = tmp;
+            }
+        }
+
+        bool empty() const
+        {
+            return first == nullptr;
+        }
+
+    };
+
 }
