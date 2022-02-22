956e6582e30b5a2edfa4159fb09b231e31714b75
deltadev99 <delta.deltadev.127@gmail.com>
2022-02-22 02:40:07 -0500

    Stats allocator now works

--- allocators/include/allocators/composite/stats.hpp
+++ allocators/include/allocators/composite/stats.hpp
@@ -13,90 +13,90 @@ namespace dd99::memory::block_allocator::composite
         { }
 
     public:
+        struct Stats_Data
+        {
+            enum Counted_Field
+            {
+                Allocation,
+                Failed_Allocation,
+                Deallocation,
+                Missed_Deallocation, // when you try to deallocate memory that does not belong to the allocator
+                Full_Deallocation,
+                Allocated_Size,
+                Deallocated_Size,
+                Field_Count,
+            };
+
+            std::size_t total[Counted_Field::Field_Count] = {};
+            double mean_allocation_request_size = 0;
+        };
+
+    public:
         [[nodiscard]]
         memory::Block allocate(std::size_t requested_size)
         {
-            m_mean_allocation_size = (m_mean_allocation_size * m_number_of_allocations + requested_size) / (m_number_of_allocations + 1);
-            ++m_number_of_allocations;
+            const auto total_previous_requested_size = m_stats_data.mean_allocation_request_size * m_stats_data.total[Stats_Data::Allocation];
+            m_stats_data.mean_allocation_request_size = (total_previous_requested_size + requested_size) / (m_stats_data.total[Stats_Data::Allocation] + 1);
+
+            ++m_stats_data.total[Stats_Data::Allocation];
 
             auto r = Sub_Alloc_T::allocate(requested_size);
 
             if (r)
-            {
-                ++m_number_of_successful_allocations;
-                m_accumulated_allocated_size += r.size;
-                m_currently_allocated_size += r.size;
-            }
+                m_stats_data.total[Stats_Data::Allocated_Size] += r.size;
+            else
+                ++m_stats_data.total[Stats_Data::Failed_Allocation];
 
             return r;
         }
 
         void deallocate(const memory::Block &memory)
         {
-            ++m_number_of_deallocations;
+            ++m_stats_data.total[Stats_Data::Deallocation];
 
-            if (Sub_Alloc_T::owns(memory))
+            if (!Sub_Alloc_T::owns(memory))
             {
-                Sub_Alloc_T::deallocate(memory);
-
-                m_currently_allocated_size -= memory.size;
-                ++m_number_of_owned_memory_deallocations;
+                ++m_stats_data.total[Stats_Data::Missed_Deallocation];
+                return;
             }
+
+            Sub_Alloc_T::deallocate(memory);
+
+            m_stats_data.total[Stats_Data::Deallocated_Size] += memory.size;            
         }
 
         void deallocate_all()
         {
+            const auto currently_allocated_size = m_stats_data.total[Stats_Data::Allocated_Size] - m_stats_data.total[Stats_Data::Deallocated_Size];
+
             Sub_Alloc_T::deallocate_all();
 
-            m_currently_allocated_size = 0;
-            ++m_number_of_deallocations;
-            ++m_number_of_owned_memory_deallocations;
+            m_stats_data.total[Stats_Data::Deallocated_Size] += currently_allocated_size;
+            ++m_stats_data.total[Stats_Data::Full_Deallocation];
         }
 
         bool owns(void *memory) const { return Sub_Alloc_T::owns(memory); }
         bool owns(const memory::Block &memory) const { return Sub_Alloc_T::owns(memory); }
 
     private:
-        std::size_t m_number_of_allocations = 0;
-        std::size_t m_number_of_successful_allocations = 0;
-        std::size_t m_accumulated_allocated_size = 0;
-        std::size_t m_number_of_deallocations = 0;
-        std::size_t m_number_of_owned_memory_deallocations = 0; // number of deallocations of memory not owned by the allocator
-        std::size_t m_currently_allocated_size = 0;
-        double m_mean_allocation_size = 0;
+        Stats_Data m_stats_data;
 
     public:
+        const Stats_Data & get_stats() const
+        { return m_stats_data; }
+
         void reset_stats()
         {
-            m_number_of_allocations = 0;
-            m_number_of_successful_allocations = 0;
-            m_accumulated_allocated_size = 0;
-            m_number_of_deallocations = 0;
-            m_number_of_owned_memory_deallocations = 0;
-            m_currently_allocated_size = 0;
-            m_mean_allocation_size = 0;
+            m_stats_data = {};
         }
-        
-        auto get_number_of_allocations() const { return m_number_of_allocations; }
-        auto get_number_of_successful_allocations() const { return m_number_of_successful_allocations; }
-        auto get_accumulated_allocated_size() const { return m_accumulated_allocated_size; }
-        auto get_number_of_deallocations() const { return m_number_of_deallocations; }
-        auto get_number_of_owned_memory_deallocations() const { return m_number_of_owned_memory_deallocations; }
-        auto get_currently_allocated_size() const { return m_currently_allocated_size; }
-        auto get_mean_allocation_size() const { return m_mean_allocation_size; }
 
-        auto get_accumulated_deallocated_size() const
-        { return m_accumulated_allocated_size - m_currently_allocated_size; }
+        // auto get_accumulated_deallocated_size() const
+        // { return m_accumulated_allocated_size - m_currently_allocated_size; }
 
-        auto get_number_of_failed_allocations() const
-        { return m_number_of_allocations - m_number_of_successful_allocations; }
+        // auto get_number_of_failed_allocations() const
+        // { return m_number_of_allocations - m_number_of_successful_allocations; }
 
-        auto get_allocation_failure_percentage() const
-        { return static_cast<double>(get_number_of_failed_allocations()) / m_number_of_allocations; }
-
-        // does not actually report how many deallocations failed
-        // but how many times attempted to deallocate memory not owned by the allocator
-        auto get_number_of_miss_deallocations() const
-        { return m_number_of_deallocations - m_number_of_owned_memory_deallocations; }
+        // auto get_allocation_failure_percentage() const
+        // { return static_cast<double>(get_number_of_failed_allocations()) / m_number_of_allocations; }
     };
 }
