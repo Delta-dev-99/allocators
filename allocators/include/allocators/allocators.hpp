cd3e6eaf01b0d7de87a0f2fec9d9e6858fb29f56
deltadev99 <delta.deltadev.127@gmail.com>
2022-02-23 03:19:00 -0500

    Added header that includes everything

---
+++ allocators/include/allocators/allocators.hpp
@@ -0,0 +1,29 @@
+
+// First two files currently don't work
+// #include <allocators/basic/bitmap.hpp>
+// #include <allocators/basic/buddy.hpp>
+#include <allocators/basic/pool.hpp>
+#include <allocators/basic/slicing.hpp>
+#include <allocators/basic/stack.hpp>
+
+#include <allocators/borrowing/bitmap.hpp>
+#include <allocators/borrowing/buddy.hpp>
+
+#include <allocators/composite/bucketizer.hpp>
+#include <allocators/composite/fallback.hpp>
+#include <allocators/composite/owner.hpp>
+#include <allocators/composite/ref.hpp>
+#include <allocators/composite/segregator.hpp>
+#include <allocators/composite/throwing.hpp>
+
+#include <allocators/degenerate/boolean.hpp>
+#include <allocators/degenerate/constant.hpp>
+#include <allocators/degenerate/failed.hpp>
+
+#include <allocators/metrics/stats.hpp>
+#include <allocators/metrics/timing.hpp>
+
+// #include <allocators/multiblock
+
+#include <allocators/pointer/basic.hpp>
+#include <allocators/pointer/checked.hpp>
