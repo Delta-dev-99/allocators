5ade5bdd3439bd6b6b9a769e8d9d644196415639
deltadev99 <delta.deltadev.127@gmail.com>
2022-03-17 19:22:47 -0400

    Fixed a bug that invalidates unique blocks when allocator is moved

--- allocators/include/allocators/utility/unique_block.hpp
+++ allocators/include/allocators/utility/unique_block.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <allocators/internal_structures/memory_block.hpp>
+#include <allocators/basic/stack.hpp>
 
 namespace dd99::memory::block_allocator::composite
 {
@@ -9,36 +10,61 @@ namespace dd99::memory::block_allocator::composite
     // NOTE: This class is not suitable for allocator polymorphism.
     // returns Unique_Block on allocation.
     // Blocks are deallocated automatically on destruction.
-    template <class Sub_Alloc_T>
+    // NOTE: The sub allocator cannot be degenerate.
+    template <class Sub_Alloc_T = dd99::memory::block_allocator::Stack>
     class Unique_Block_Allocator
     {
     public:
+
+        // BUG: When allocator is moved, the pointer inside deallocator is invalidated
         struct Deallocator
         {
             // used ptr to allow assignment
-            Unique_Block_Allocator * allocator = nullptr;
+            Unique_Block_Allocator ** allocator = nullptr;
             
             constexpr void
             operator()(const memory::Block & memory) const
             {
-                if (allocator)
-                    allocator->deallocate(memory);
+                if (allocator && *allocator)
+                    (*allocator)->deallocate(memory);
             }
         };
 
         using Block_Type = dd99::memory::Unique_Block<Deallocator>;
 
     public:
+        ~Unique_Block_Allocator()
+        {
+            auto self_ptr_ptr = reinterpret_cast<Unique_Block_Allocator **>(m_self_ptr_block.base);
+            if (self_ptr_ptr)
+                *self_ptr_ptr = nullptr;
+            m_sub_alloc.deallocate(m_self_ptr_block);
+        }
+
         Unique_Block_Allocator(Sub_Alloc_T && sub_allocator)
             : m_sub_alloc(std::move(sub_allocator))
-        { }
+            , m_self_ptr_block(m_sub_alloc.allocate(sizeof(this)))
+        {
+            auto ptr = new (m_self_ptr_block.base) Unique_Block_Allocator *;
+            *ptr = this;
+        }
+
+        Unique_Block_Allocator(Unique_Block_Allocator && other)
+            : m_sub_alloc(std::move(other.m_sub_alloc))
+            , m_self_ptr_block(std::move(other.m_self_ptr_block))
+        {
+            *reinterpret_cast<Unique_Block_Allocator **>(m_self_ptr_block.base) = this;
+            other.m_self_ptr_block = {};
+        }
 
     public:
         [[nodiscard]]
         Block_Type
         allocate(std::size_t requested_size)
         {
-            return Block_Type{m_sub_alloc.allocate(requested_size), Deallocator{this}};
+            auto mem = m_sub_alloc.allocate(requested_size);
+            auto deallocator = Deallocator{reinterpret_cast<Unique_Block_Allocator **>(m_self_ptr_block.base)};
+            return Block_Type{std::move(mem), std::move(deallocator)};
         }
 
         void deallocate(const memory::Block &memory)
@@ -55,6 +81,7 @@ namespace dd99::memory::block_allocator::composite
     
     private:
         Sub_Alloc_T m_sub_alloc;
+        memory::Block m_self_ptr_block;
     };
 
 }
