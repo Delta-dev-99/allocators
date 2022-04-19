3b9ff18f46e47070063a43c9b7374f2bfff019f4
deltadev99 <delta.deltadev.127@gmail.com>
2022-04-19 03:32:18 -0400

    Added extended allocator reference class

---
+++ allocators/include/allocators/utility/xref.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+
+
+namespace dd99::memory::block_allocator::utility
+{
+    // Wraps a reference to an allocator.
+    // Requires an existing and persisting allocator instance.
+    // This allows the use of the same allocator as an underlying
+    // allocator of 2 or more composite allocators.
+    template <class Sub_Alloc_T>
+    class XRef
+    {
+    public:
+        XRef(Sub_Alloc_T &underlying_allocator)
+            : m_alloc_ref(underlying_allocator)
+        { }
+
+    public:
+        template <class Request>
+        [[nodiscard]]
+        memory::Block allocate(Request request)
+        { return m_alloc_ref.allocate(request); }
+
+        void deallocate(const memory::Block &memory)
+        { m_alloc_ref.deallocate(memory); }
+
+        void deallocate_all()
+        { m_alloc_ref.deallocate_all(); }
+
+        bool owns(std::byte *memory) const
+        { return m_alloc_ref.owns(memory); }
+
+        bool owns(const memory::Block &memory) const
+        { return m_alloc_ref.owns(memory); }
+
+    protected:
+        Sub_Alloc_T &m_alloc_ref;
+    };
+}
