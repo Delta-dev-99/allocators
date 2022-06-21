0e858865b6ead708c6fe362fec574340a1bf0be8
deltadev99 <delta.deltadev.127@gmail.com>
2022-06-21 02:12:46 -0400

    Added new/delete-like function templates that use allocators

---
+++ allocators/include/allocators/structures/new_result.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <allocators/structures/memory_block.hpp>
+
+namespace dd99::memory
+{
+    // The result of an allocator-new
+    template <class T>
+    struct new_result
+    {
+        T * pointer = nullptr;
+        std::size_t size = 0;
+
+        constexpr new_result() noexcept = default;
+        constexpr explicit new_result(const dd99::memory::Block block) noexcept
+            : pointer(reinterpret_cast<T *>(block.base))
+            , size(block.size)
+        { }
+
+        // conversion to memory block
+        operator dd99::memory::Block()
+        { return {reinterpret_cast<std::byte *>(pointer), size}; }
+
+        // decay to pointer
+        operator T *()
+        { return pointer; }
+
+        // operator *
+        constexpr T & operator*()
+        { return *pointer; }
+
+        // operator ->
+        constexpr T * operator->()
+        { return pointer; }
+    };
+}
